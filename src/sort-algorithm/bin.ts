46b57007b71b373cc08c026db5c9443e957faa64
ato lash <28973861+hal-shu-sato@users.noreply.github.com>
2024-06-03 07:44:32 +0900

    fix bin sort

--- src/sort-algorithm/bin.ts
+++ src/sort-algorithm/bin.ts
@@ -1,5 +1,7 @@
 const binSort: SortFunc = (a, size) => {
-  const m = Math.max(...a);
+  const count = { copy: 0 };
+
+  const m = a.reduce((a, b) => Math.max(a, b), -Infinity);
 
   const head = new Array<number>(m);
   for (let i = 0; i < m; i++) {
@@ -9,6 +11,7 @@ const binSort: SortFunc = (a, size) => {
   const next = new Array<number>(size);
   for (let i = size - 1; i >= 0; i--) {
     const value = a[i];
+    count.copy++;
     next[i] = head[value - 1];
     head[value - 1] = i;
   }
@@ -18,14 +21,18 @@ const binSort: SortFunc = (a, size) => {
   for (let j = 0; j < m; j++) {
     let p = head[j];
     while (p !== -1) {
+      count.copy++;
       b[i++] = a[p];
       p = next[p];
     }
   }
 
   for (let i = 0; i < size; i++) {
+    count.copy++;
     a[i] = b[i];
   }
+
+  console.log("コピー回数", count.copy);
 };
 
 export default binSort;
